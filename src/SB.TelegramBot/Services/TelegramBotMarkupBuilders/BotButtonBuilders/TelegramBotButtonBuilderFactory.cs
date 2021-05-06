43ca721f50cc38632d331da457055702460a2c93
HP <HP@DESKTOP-GDLLM62>
2021-05-06 19:11:51 +0500

    ITelegramBotButtonBuilderFactory added

---
+++ src/SB.TelegramBot/Services/TelegramBotMarkupBuilders/BotButtonBuilders/TelegramBotButtonBuilderFactory.cs
@@ -0,0 +1,43 @@
+﻿using SB.TelegramBot.Logics.TelegramBotCommands.Factories;
+
+namespace SB.TelegramBot
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [BotSingletonService]
+    public class TelegramBotButtonBuilderFactory : ITelegramBotButtonBuilderFactory
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private readonly ITelegramBotCommandFactory _commandFactory;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="commandFactory"></param>
+        public TelegramBotButtonBuilderFactory(ITelegramBotCommandFactory commandFactory)
+        {
+            _commandFactory = commandFactory;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public KeyboardButtonBuilder CreateKeyboardButtonBuilder()
+        {
+            return new KeyboardButtonBuilder();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public InlineKeyboardButtonBuilder CreateInlineKeyboardButtonBuilder()
+        {
+            return new InlineKeyboardButtonBuilder(_commandFactory);
+        }
+    }
+}
